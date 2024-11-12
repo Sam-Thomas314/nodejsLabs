73249120547bd55fd67717b2166c393ec9698205
Sam-Thomas314 <Samuelajt@gmail.com>
2024-11-12 14:55:27 -0500

    Small changes

--- routes/users.js
+++ routes/users.js
@@ -26,20 +26,62 @@ let users = [
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify({users}, null, 4))//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
-router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.put("/:email", (req, res) => {
+    // Extract email parameter and find users with matching email
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    
+    if (filtered_users.length > 0) {
+        // Select the first matching user and update attributes if provided
+        let filtered_user = filtered_users[0];
+        
+         // Extract and update DOB if provided
+        
+        let DOB = req.query.DOB;    
+        if (DOB) {
+            filtered_user.DOB = DOB;
+        }
+        let new_first_name = req.query.firstName;    
+        if (new_first_name) {
+            filtered_user.firstName = new_first_name;
+        }
+        
+        let lastName = req.query.lastName;    
+        if (lastName) {
+            filtered_user.lastName = lastName;
+        }
+        
+        // Replace old user entry with updated user
+        users = users.filter((user) => user.email != email);
+        //take out user
+        users.push(filtered_user);
+        //add user back into the new array we created when we took him out
+        //add him back in with the changes
+
+        // Send success message indicating the user has been updated
+        res.send(`User with the email ${email} updated.`);
+    } else {
+        // Send error message if no user found
+        res.send("Unable to find user!");
+    }
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    // Push a new user object into the users array based on query parameters from the request
+    users.push({
+        "firstName": req.query.firstName,
+        "lastName": req.query.lastName,
+        "email": req.query.email,
+        "DOB": req.query.DOB
+    });
+    // Send a success message as the response, indicating the user has been added
+    res.send("The user " + req.query.firstName + " has been added!");
 });
 
 
@@ -52,8 +94,12 @@ router.put("/:email", (req, res) => {
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    // Extract the email parameter from the request URL
+    const email = req.params.email;
+    // Filter the users array to exclude the user with the specified email
+    users = users.filter((user) => user.email != email);
+    // Send a success message as the response, indicating the user has been deleted
+    res.send(`User with the email ${email} deleted.`);
 });
 
 module.exports=router;
